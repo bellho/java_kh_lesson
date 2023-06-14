d1134fc76e3e0490d567805be2fe7fc2aa2448fc
bellho <1313kjh@gmail.com>
2023-06-14 10:06:44 +0900

    23.06.14 multichat 실습_1

---
+++ lesson/20230614_multichat/src/kh/lclass/chat/client/ClientRun.java
@@ -0,0 +1,7 @@
+package kh.lclass.chat.client;
+
+public class ClientRun {
+public static void main(String[] args) {
+	//TODO
+}
+}
---
+++ lesson/20230614_multichat/src/kh/lclass/chat/server/ServerGUI.java
@@ -0,0 +1,7 @@
+package kh.lclass.chat.server;
+
+import javax.swing.JFrame;
+
+public class ServerGUI extends JFrame {
+
+}
